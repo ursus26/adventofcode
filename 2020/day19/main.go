b3dbaaa1ef58a657e06f236bd067c9c0a4f80465
Bart de Haan <bart.dehaan@hotmail.com>
2021-07-20 17:15:35 +0200

    Added 2020 day 19

--- 2020/day19/main.go
+++ 2020/day19/main.go
@@ -5,119 +5,60 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
-const (
-	ITEM_RULE      = iota
-	ITEM_CHARACTER = iota
-)
-
-var rules []Rule
-
-type Requirement struct {
-	requirementType int
-	value           int
-}
-
-type Expression struct {
-	items []Requirement
-}
-
-type Rule struct {
-	id          int
-	expressions []Expression
-}
-
-func NewRule(id int) Rule {
-	rule := Rule{
-		id:          id,
-		expressions: []Expression{},
+func parseRuleToRegexString(ruleID int, parsedRules map[int]string, unparsedRules map[int]string, part2 bool) string {
+	// Check if we have already parsed the rule.
+	result, ok := parsedRules[ruleID]
+	if ok {
+		return result
+	} else {
+		result = ""
 	}
-	return rule
-}
 
-func parseRule(line string) Rule {
-	words := strings.Split(line, ": ")
+	expression := unparsedRules[ruleID]
+	orExpression := false
+	for _, requirement := range strings.Split(expression, " ") {
+		if requirement[0] == 'a' || requirement[0] == 'b' {
+			result += requirement
+		} else if requirement[0] == '|' {
+			orExpression = true
+			result += "|"
+		} else {
+			value, _ := strconv.Atoi(requirement)
 
-	ruleID, _ := strconv.Atoi(words[0])
-	newRule := NewRule(ruleID)
-
-	requirements := strings.Split(words[1], " | ")
-	for _, requirementList := range requirements {
-		expr := Expression{}
-		for _, requirement := range strings.Split(requirementList, " ") {
-			if requirement[0] == '"' {
-				character := -1
-				if requirement[1] == 'a' {
-					character = 0
-				} else if requirement[1] == 'b' {
-					character = 1
-				}
-				item := Requirement{
-					requirementType: ITEM_CHARACTER,
-					value:           character,
-				}
-				expr.items = append(expr.items, item)
+			if part2 && value == 8 {
+				result += parseRuleToRegexString(42, parsedRules, unparsedRules, part2) + "+"
 				continue
-			} else {
-				value, _ := strconv.Atoi(requirement)
-				item := Requirement{
-					requirementType: ITEM_RULE,
-					value:           value,
+			} else if part2 && value == 11 {
+				rule42 := parseRuleToRegexString(42, parsedRules, unparsedRules, part2)
+				rule31 := parseRuleToRegexString(31, parsedRules, unparsedRules, part2)
+				maxLoops := 5
+				result += "("
+				for i := 0; i < maxLoops-1; i++ {
+					result += strings.Repeat(rule42, i+1) + strings.Repeat(rule31, i+1) + "|"
 				}
-				expr.items = append(expr.items, item)
+				result += strings.Repeat(rule42, maxLoops) + strings.Repeat(rule31, maxLoops) + ")"
+				continue
+			}
+
+			regexExpression, ok := parsedRules[value]
+			if ok {
+				result += regexExpression
+			} else {
+				result += parseRuleToRegexString(value, parsedRules, unparsedRules, part2)
 			}
 		}
-		newRule.expressions = append(newRule.expressions, expr)
-	}
-	return newRule
-}
-
-func matchRule(ruleID int, word []int) (int, bool) {
-	rule := rules[ruleID]
-	ruleMatched := false
-	matchedCharacters := 0
-	for _, expression := range rule.expressions {
-		count, ok := matchExpression(expression, word)
-		if ok {
-			matchedCharacters = count
-			ruleMatched = true
-			break
-		}
 	}
 
-	return matchedCharacters, ruleMatched
-}
-
-func matchExpression(expr Expression, word []int) (int, bool) {
-	matchedCharacters := 0
-	for _, item := range expr.items {
-		characterCount, ok := matchRequirement(item, word[matchedCharacters:])
-		if !ok {
-			return 0, false
-		}
-		matchedCharacters += characterCount
+	if orExpression {
+		result = "(" + result + ")"
 	}
-	return matchedCharacters, true
-}
-
-func matchRequirement(requirement Requirement, word []int) (int, bool) {
-	switch requirement.requirementType {
-	case ITEM_CHARACTER:
-		if len(word) == 0 {
-			return 0, false
-		} else if word[0] != requirement.value {
-			return 0, false
-		} else {
-			return 1, true
-		}
-	case ITEM_RULE:
-		return matchRule(requirement.value, word)
-	default:
-		return 0, false
-	}
+	parsedRules[ruleID] = result
+	return result
 }
 
 func main() {
@@ -129,70 +70,44 @@ func main() {
 
 	/* Data parsing. */
 	bytes, err := ioutil.ReadAll(file)
-	data := strings.Split(strings.Trim(string(bytes), "\n"), "\n\n")
-	unparsedRules := strings.Split(data[0], "\n")
+	stringData := string(bytes)
+	data := strings.Split(strings.Trim(strings.ReplaceAll(stringData, "\"", ""), "\n"), "\n\n")
+	ruleLines := strings.Split(data[0], "\n")
 
-	rules = make([]Rule, 1024)
-	for _, unparsedRule := range unparsedRules {
-		newRule := parseRule(unparsedRule)
-		rules[newRule.id] = newRule
+	unparsedRules := make(map[int]string)
+	for _, line := range ruleLines {
+		words := strings.Split(line, ": ")
+		ruleID, _ := strconv.Atoi(words[0])
+		unparsedRules[ruleID] = words[1]
 	}
+
+	parsedRules := make(map[int]string)
+	regexString := "^" + parseRuleToRegexString(0, parsedRules, unparsedRules, false) + "$"
+	regexPart1 := regexp.MustCompile(regexString)
 
 	/* Part 1 */
 	part1 := 0
 	lines := strings.Split(data[1], "\n")
 	for _, line := range lines {
-		word := make([]int, len(line))
-		for i, character := range line {
-			switch character {
-			case 'a':
-				word[i] = 0
-			case 'b':
-				word[i] = 1
-			default:
-				word[i] = -1
-			}
-		}
-		matchedCharacters, valid := matchRule(0, word)
-		if matchedCharacters != len(word) {
-			valid = false
-		}
-
-		if valid {
+		if regexPart1.MatchString(line) {
 			part1 += 1
 		}
 	}
 
-	/* Part 2 */
-	rule8 := parseRule("8: 42 | 42 8")
-	rule11 := parseRule("11: 42 31 | 42 11 31")
-	rules[8] = rule8
-	rules[11] = rule11
+	delete(parsedRules, 0)
+	delete(parsedRules, 8)
+	delete(parsedRules, 11)
+	regexString2 := "^" + parseRuleToRegexString(0, parsedRules, unparsedRules, true) + "$"
+	regexPart2 := regexp.MustCompile(regexString2)
 
+	/* Part 2 */
 	part2 := 0
 	for _, line := range lines {
-		word := make([]int, len(line))
-		for i, character := range line {
-			switch character {
-			case 'a':
-				word[i] = 0
-			case 'b':
-				word[i] = 1
-			default:
-				word[i] = -1
-			}
-		}
-		matchedCharacters, valid := matchRule(0, word)
-		if matchedCharacters != len(word) {
-			valid = false
-		}
-
-		if valid {
-			// fmt.Printf("PART2 | Valid word: %s\n", line)
+		if regexPart2.MatchString(line) {
 			part2 += 1
 		}
 	}
 
 	fmt.Printf("Day 19 part 1, answer: %d\n", part1)
-	fmt.Printf("Part 2 is not finished\nDay 19 part 2, answer: %d\n", part2)
+	fmt.Printf("Day 19 part 2, answer: %d\n", part2)
 }
